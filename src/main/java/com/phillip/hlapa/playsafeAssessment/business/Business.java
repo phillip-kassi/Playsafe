0791ae38370c4d8e423421d4d2975c46e9aa5c3a
Phillip Hlapa <phillip.hlapa@momentum.co.za>
2020-08-10 10:17:44 +0200

    methods to convert from kelvin to celsius and virsa versa

--- src/main/java/com/phillip/hlapa/playsafeAssessment/business/Business.java
+++ src/main/java/com/phillip/hlapa/playsafeAssessment/business/Business.java
@@ -1,5 +1,14 @@
 package com.phillip.hlapa.playsafeAssessment.business;
 
+import com.phillip.hlapa.playsafeAssessment.constants.Constans;
+
 public class Business {
 
+	//conversions/ktoc
+	public double kelvinToCelsius(double kelvin) {
+		return  kelvin - Constans.KELVINE_CONSTANT;
+	}
+	public double celsiusToKelvin(double celsius) {
+		return  celsius + Constans.KELVINE_CONSTANT;
+	}
 }
---
+++ src/main/java/com/phillip/hlapa/playsafeAssessment/constants/Constans.java
@@ -0,0 +1,7 @@
+package com.phillip.hlapa.playsafeAssessment.constants;
+
+public interface Constans {
+
+	double KELVINE_CONSTANT = 273.15;
+
+}

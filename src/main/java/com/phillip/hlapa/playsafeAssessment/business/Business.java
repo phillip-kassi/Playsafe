7f511ce4cb50dd4f82fb1a0ac88673ad2c6d0b2a
Phillip Hlapa <phillip.hlapa@momentum.co.za>
2020-08-10 13:14:11 +0200

    added logginh

--- src/main/java/com/phillip/hlapa/playsafeAssessment/business/Business.java
+++ src/main/java/com/phillip/hlapa/playsafeAssessment/business/Business.java
@@ -1,26 +1,39 @@
 package com.phillip.hlapa.playsafeAssessment.business;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.phillip.hlapa.playsafeAssessment.constants.Constans;
 
 public class Business {
 
+	private Logger logger = LoggerFactory.getLogger(Business.class);
+
+	public Business() {
+		logger.info("initializing Business method to perform operations");
+	}
+
 	//convert Kelvin to Celsius
 	public double kelvinToCelsius(double kelvin) {
+		logger.info("execution of method: KelvineToCelsius in Business");
 		return  kelvin - Constans.KELVIN_CONSTANT;
 	}
 	
 	//convert Celsius to Kelvin
 	public double celsiusToKelvin(double celsius) {
+		logger.info("execution of method: CelsiusToKelvin in Business");
 		return  celsius + Constans.KELVIN_CONSTANT;
 	}
 	
 	//Convert Miles to Kilometres
 	public double milesToKilometres(double miles) {
+		logger.info("execution of method: MilesToKilometres in Business");
 		return miles * Constans.MILES_TO_KILOMETRES_CONSTANT;
 	}
 	
 	//Convert Kilometres to Miles
 	public double kilometresToMiles(double kilometers) {
+		logger.info("execution of method: KilometresToMiles in Business");
 		return kilometers * Constans.KILOMETRES_TO_MILES_CONSTANT;
 	}
 }
--- src/main/java/com/phillip/hlapa/playsafeAssessment/service/Service.java
+++ src/main/java/com/phillip/hlapa/playsafeAssessment/service/Service.java
@@ -1,5 +1,7 @@
 package com.phillip.hlapa.playsafeAssessment.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,40 +12,50 @@ import com.phillip.hlapa.playsafeAssessment.constants.Constans;
 public class Service {
 
 	private Business business = null;
+	private static final Logger LOGGER = LoggerFactory.getLogger(Service.class);
 	
 	public Service() {
-		//initialize business object
 		business = new Business();
 	}
-	
+
 	//REST Services
 	//http://localhost:8088/conversions/ktoc?kelvin=6
 	@GetMapping("/conversions/ktoc")
 	public String toCelsius(@RequestParam String kelvin) {
+		LOGGER.info("start of method: toCelsius. Convert kelvin to celsius. INPUT: " + kelvin);
 		String result =  business.kelvinToCelsius(Double.parseDouble(kelvin.trim())) + "";
+		LOGGER.info("end of method: toCelsius. Convert kelvin to celsius. OUTPUT: " + result);
 		return toHtml(result, Constans.KELVIN_TO_CELSIUS_ENDPOINT);
+		
 	}
 	//http://localhost:8088/conversions/ctok?celsius=6
 	@GetMapping("/conversions/ctok") 
 	public String toKelvin(@RequestParam String celsius) {
+		LOGGER.info("start of method: toKelvin. Convert celsius to kelvin. INPUT: " + celsius);
 		String result = business.celsiusToKelvin(Double.parseDouble(celsius.trim())) + "";
+		LOGGER.info("end of method: toKelvin. Convert celsius to kelvin. OUTPUT: " + result);
 		return toHtml(result, Constans.CELSIUS_TO_KELVIN_ENDPOINT);
 	}
 	//http://localhost:8088/conversions/mtok?miles=6
 	@GetMapping("/conversions/mtok") 
 	public String toKilometres(@RequestParam String miles) {
+		LOGGER.info("start of method: toKilometres. Convert Miles to Kilometres. INPUT: " + miles);
 		String result = business.milesToKilometres(Double.parseDouble(miles.trim())) + "";
+		LOGGER.info("end of method: toKilometres. Convert Miles to Kilometres. OUTPUT: " + result);
 		return toHtml(result, Constans.MILES_TO_KILOMETRES_ENDPOINT);
 	}
 	//http://localhost:8088/conversions/ktom?kilometres=6
 	@GetMapping("/conversions/ktom") 
 	public String toMiles(@RequestParam String kilometres) {
+		LOGGER.info("start of method: toKilometres. Convert Miles to Kilometres. INPUT: " + kilometres);
 		String result = business.kilometresToMiles(Double.parseDouble(kilometres.trim())) + "";
+		LOGGER.info("end of method: toKilometres. Convert Miles to Kilometres. OUTPUT: " + result);
 		return toHtml(result, Constans.KILOMETRES_TO_MILES_ENDPOINT);
 	}
-	
+
 	//Helper method
 	private String toHtml(String result, String endpoint) {
+		LOGGER.info("start of method: toHTML...");
 		StringBuffer sb = new StringBuffer();
 		sb.append("<h3>");
 		switch (endpoint) {
@@ -63,6 +75,7 @@ public class Service {
 			break;
 		}
 		sb.append("</h3>");
+		LOGGER.warn("end of method: toHTML. ENDPOINT: /conversions/" +endpoint+ ". OUTPUT: " + sb.toString());
 		return sb.toString();
 	}
 }

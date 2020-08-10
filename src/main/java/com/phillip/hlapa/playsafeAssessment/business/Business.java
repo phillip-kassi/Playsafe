6402ac577fc48874fc9a37379d183bdd8a81c95f
Phillip Hlapa <phillip.hlapa@momentum.co.za>
2020-08-10 10:38:15 +0200

    methods to convert from Kilometres to Miles and virsa versa

--- src/main/java/com/phillip/hlapa/playsafeAssessment/business/Business.java
+++ src/main/java/com/phillip/hlapa/playsafeAssessment/business/Business.java
@@ -4,11 +4,23 @@ import com.phillip.hlapa.playsafeAssessment.constants.Constans;
 
 public class Business {
 
-	//conversions/ktoc
+	//convert Kelvin to Celsius
 	public double kelvinToCelsius(double kelvin) {
-		return  kelvin - Constans.KELVINE_CONSTANT;
+		return  kelvin - Constans.KELVIN_CONSTANT;
 	}
+	
+	//convert Celsius to Kelvin
 	public double celsiusToKelvin(double celsius) {
-		return  celsius + Constans.KELVINE_CONSTANT;
+		return  celsius + Constans.KELVIN_CONSTANT;
+	}
+	
+	//COnvert Miles to Kilometres
+	public double milesToKilometres(double miles) {
+		return miles * Constans.MILES_TO_KILOMETRES;
+	}
+	
+	//Convert Kilometres to Miles
+	public double kilometresToMiles(double kilometers) {
+		return kilometers * Constans.KILOMETRES_TO_MILES;
 	}
 }
--- src/main/java/com/phillip/hlapa/playsafeAssessment/constants/Constans.java
+++ src/main/java/com/phillip/hlapa/playsafeAssessment/constants/Constans.java
@@ -2,6 +2,8 @@ package com.phillip.hlapa.playsafeAssessment.constants;
 
 public interface Constans {
 
-	double KELVINE_CONSTANT = 273.15;
+	double KELVIN_CONSTANT = 273.15;
+	double MILES_TO_KILOMETRES = 1.609;
+	double KILOMETRES_TO_MILES = 0.621;
 
 }
